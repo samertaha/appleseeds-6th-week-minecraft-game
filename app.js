6d574dabd86e2c28742bc429ca3e780148ce5e87
Samer taha <alrazy1@gmail.com>
2022-04-30 13:46:51 +0300

    added tools functionality

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
 const { log } = console;
 
+let currentTool = 'PICKAXE';
+toolMode = true;
+let bankStack = [];
+
 const pickaxe = document.querySelector('.tool[PICKAXE]');
 
 pickaxe.addEventListener('click', function () {
-  alert(' PICKAXE tool clicked');
+  toolMode = true;
+  currentTool = 'PICKAXE';
 });
 const shovel = document.querySelector('.tool[SHOVEL]');
 
 shovel.addEventListener('click', function () {
-  alert(' shovel tool clicked');
+  toolMode = true;
+  currentTool = 'SHOVEL';
 });
 const axe = document.querySelector('.tool[AXE]');
 
 axe.addEventListener('click', function () {
-  alert(' AXE tool clicked');
+  toolMode = true;
+  currentTool = 'AXE';
 });
+
+const bank = document.querySelector('.inventory[bank]');
+
+bank.addEventListener('click', function () {
+  toolMode = false;
+  alert(' bank tool clicked');
+});
+
+const cells = document.querySelectorAll('.world .row div');
+
+cells.forEach((cell) =>
+  cell.addEventListener('click', function () {
+    if (toolMode) {
+      const itsLeaf = this.getAttribute('leafs') !== null;
+      const itsTreetrunk = this.getAttribute('treetrunk') !== null;
+      const itsStone = this.getAttribute('stone') !== null;
+      const itsGrass = this.getAttribute('grass') !== null;
+      const itsGround = this.getAttribute('ground') !== null;
+
+      switch (currentTool) {
+        case 'PICKAXE':
+          if (itsStone) this.removeAttribute('stone');
+          break;
+        case 'SHOVEL':
+          if (itsGrass) this.removeAttribute('grass');
+          if (itsGround) this.removeAttribute('ground');
+          break;
+        case 'AXE':
+          if (itsLeaf) this.removeAttribute('leafs');
+          if (itsTreetrunk) this.removeAttribute('treetrunk');
+          break;
+        default:
+          console.log('no such tool!');
+      }
+    } else {
+      // bank mode
+    }
+  })
+);
